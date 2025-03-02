376348459e4ddeeb3ae85730949fb8ee8854a9d4
Nisha <nishubharti98@gmail.com>
2025-03-02 18:55:04 +0530

    TEMPORARY TABLES and CLONE TABLES

--- Transaction.sql
+++ Transaction.sql
@@ -40,52 +40,92 @@ CREATE TABLE accounts (
 
 -- COMMIT;
 
-SET AUTOCOMMIT = 0;  -- Turns off auto-commit
-START TRANSACTION;
+-- SET AUTOCOMMIT = 0;  -- Turns off auto-commit
+-- START TRANSACTION;
 
-UPDATE accounts SET balance = balance - 300 WHERE account_id = 1;
-UPDATE accounts SET balance = balance + 300 WHERE account_id = 2;
+-- UPDATE accounts SET balance = balance - 300 WHERE account_id = 1;
+-- UPDATE accounts SET balance = balance + 300 WHERE account_id = 2;
 
-COMMIT;
+-- COMMIT;
 
-SELECT * FROM accounts;
+-- SELECT * FROM accounts;
 
-START TRANSACTION;
+-- START TRANSACTION;
 
-insert into accounts(account_id, name, balance) values (3,'nisha', 2000); 
-UPDATE accounts SET balance = balance - 300 WHERE account_id = 4;
+-- insert into accounts(account_id, name, balance) values (3,'nisha', 2000); 
+-- UPDATE accounts SET balance = balance - 300 WHERE account_id = 4;
 
 
-SET AUTOCOMMIT =0;
-START TRANSACTION;
+-- SET AUTOCOMMIT =0;
+-- START TRANSACTION;
 
-insert into accounts(name, balance) values ('neha', 200); 
-UPDATE accounts SET balance = balance - 300 WHERE account_id = 8;
+-- insert into accounts(name, balance) values ('neha', 200); 
+-- UPDATE accounts SET balance = balance - 300 WHERE account_id = 8;
 
-COMMIT;
+-- COMMIT;
 
--- class 2 
+-- -- class 2 
 
-SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
-START TRANSACTION;
+-- SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+-- START TRANSACTION;
 
-insert into accounts(name, balance) values ('neha', 200); 
-UPDATE accounts SET balance = balance - 300 WHERE account_id = 8;
+-- insert into accounts(name, balance) values ('neha', 200); 
+-- UPDATE accounts SET balance = balance - 300 WHERE account_id = 8;
 
-COMMIT;
+-- COMMIT;
 
-SELECT * FROM accounts;
+-- SELECT * FROM accounts;
 
 -- shared locks
 
-SELECT * FROM accounts
-WHERE account_id = 1
-lock in share mode;
+-- SELECT * FROM accounts
+-- WHERE account_id = 1
+-- lock in share mode;
 
--- Ecxclusive locks
-SELECT * FROM accounts
-WHERE account_id = 1
-FOR UPDATE;
+-- unlock 
+-- -- Ecxclusive locks
+-- SELECT * FROM accounts
+-- WHERE account_id = 1
+-- FOR UPDATE;
+
+-- class 3
+
+create  temporary Table Temp_users (
+ID int auto_increment primary key,
+name varchar(50)
+);
+
+insert into Temp_users (name) values('nisha');
+select * from Temp_users;
+
+drop temporary table if exists Temp_users;
+
+SELECT * FROM accounts;
+
+create table new_table as Select * from accounts;
+
+select * from new_table;
+
+create table accountstructure like accounts;
+
+select * from accountstructure;
+
+select count(*) from accounts;
+
+show databases;
+
+show tables;
+
+desc accounts;
+
+select * from information_schema.Tables where table_schema ='bank';
 
 
+select * from accounts;
+
+show create table accounts;
+
+alter table accounts auto_increment=100;
+
+select * from accounts;
 
